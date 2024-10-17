4bf1ac9880a7bb9d485e655215428a9eece4da6e
RichieQuintana <103153905+RichieQuintana@users.noreply.github.com>
2024-10-16 19:38:02 -0500

    Update main.py

--- LOGIN/main.py
+++ LOGIN/main.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, cur
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta"
-6
+
 # Configuración de SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
